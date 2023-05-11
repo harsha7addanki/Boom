40b577aef407e548fc874d71e879c07233cba278
Harsha Addanki <harsha7addanki@gmail.com>
2023-05-10 20:24:37 -0400

    modified:   dist/bundle.js 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
-  module: {
+    entry: './src/index.ts',
+    module: {
     rules: [
-      {
+        {
         test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
+        },
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-  },
-  optimization: {
-    minimize: false
-},
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: "BoomJS",
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
 };
